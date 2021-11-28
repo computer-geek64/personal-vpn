90afd4c281e58c7fef8aa9928f164bb5d2a8ccb9
Ashish D'Souza <sudouser512@gmail.com>
2021-11-27 21:05:26 -0500

    Added configuration file

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@
 
 FROM kylemanna/openvpn
 WORKDIR /code
-RUN apk add curl
+RUN apk update
+RUN apk add curl openssh
 COPY . .
-ARG vpn_server_ip=127.0.0.1
-ENV VPN_SERVER_IP=${vpn_server_ip}
+ARG vpn_server_addr=127.0.0.1
+ENV VPN_SERVER_ADDR=${vpn_server_addr}
 ARG vpn_server_port=1194
 ENV VPN_SERVER_PORT=${vpn_server_port}
 CMD test -z "$(ls -A /etc/openvpn)" && src/init.sh; ovpn_run
--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # init.sh
 
-test "${VPN_SERVER_IP}" = "127.0.0.1" && VPN_SERVER_IP=$(curl -s http://ipecho.net/plain)
-ovpn_genconfig -u tcp://${VPN_SERVER_IP}:${VPN_SERVER_PORT}
+test "${VPN_SERVER_ADDR}" = "127.0.0.1" && VPN_SERVER_ADDR=$(curl -s http://ipecho.net/plain)
+
+ovpn_genconfig -u tcp://${VPN_SERVER_ADDR}:${VPN_SERVER_PORT}
+sed -i '/push "dhcp-option DNS .*"/d;/push "block-outside-dns"/a push "dhcp-option DNS 192.168.255.2"' /etc/openvpn/openvpn.conf
+cp src/homebase /etc/openvpn/ccd/homebase
+
 ovpn_initpki
