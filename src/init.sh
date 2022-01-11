191ed63db922f7ae5c05d419f2a9cee1b8b7d420
Ashish D'Souza <sudouser512@gmail.com>
2022-01-11 14:49:52 -0500

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -5,6 +5,6 @@ test "${VPN_SERVER_ADDR}" = "127.0.0.1" && VPN_SERVER_ADDR=$(curl -s http://ipec
 
 ovpn_genconfig -u tcp://${VPN_SERVER_ADDR}:${VPN_SERVER_PORT}
 sed -i '/push "dhcp-option DNS .*"/d;/push "block-outside-dns"/a push "dhcp-option DNS 192.168.255.2"' /etc/openvpn/openvpn.conf
-cp src/homebase /etc/openvpn/ccd/homebase
+cp src/pi_hole /etc/openvpn/ccd/pi_hole
 
 ovpn_initpki
